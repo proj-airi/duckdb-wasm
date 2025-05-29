4b27ea9ae35a8f65110bd97232642508f1b66017
Neko Ayaka <neko@ayaka.moe>
2025-05-29 19:27:27 +0800

    fix(drizzle-duckdb-wasm,duckdb-wasm): export more data

--- packages/drizzle-duckdb-wasm/src/index.ts
+++ packages/drizzle-duckdb-wasm/src/index.ts
@@ -2,7 +2,34 @@ export * from './driver'
 export * from './dsn'
 export * from './migrator'
 export * from './session'
-export type { AsyncDuckDBConnection, DuckDBBundles, Logger } from '@duckdb/duckdb-wasm'
-export { AsyncDuckDB, ConsoleLogger, getJsDelivrBundles, selectBundle, VoidLogger } from '@duckdb/duckdb-wasm'
-export type { ConnectOptionalOptions, ConnectOptions, ConnectRequiredOptions, DuckDBWasmClient } from '@proj-airi/duckdb-wasm'
-export { connect, mapColumnData } from '@proj-airi/duckdb-wasm'
+export type {
+  AsyncDuckDBConnection,
+  DuckDBBundles,
+  Logger,
+} from '@duckdb/duckdb-wasm'
+export {
+  AsyncDuckDB,
+  ConsoleLogger,
+  getJsDelivrBundles,
+  selectBundle,
+  VoidLogger,
+} from '@duckdb/duckdb-wasm'
+export type {
+  ArrowDataType,
+  ArrowField,
+  ArrowSchema,
+  ArrowTable,
+  ArrowTypeMap,
+  ConnectOptionalOptions,
+  ConnectOptions,
+  ConnectRequiredOptions,
+  DataType,
+  DuckDBWasmClient,
+  ResultColumns,
+} from '@proj-airi/duckdb-wasm'
+export {
+  connect,
+  mapColumnData,
+  mapStructRowData,
+  withSavepoint,
+} from '@proj-airi/duckdb-wasm'
